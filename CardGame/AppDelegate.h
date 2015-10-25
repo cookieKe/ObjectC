f9405b5783f56b1bd25fb9370a4f6004e942f75c
cookieKe <cyneuzk@gmail.com>
2015-10-25 11:06:00 +0800

    Initial Commit

---
+++ CardGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CardGame
+//
+//  Created by Kevin Zhu on 15/10/25.
+//  Copyright (c) 2015年 Kevin Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CardGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CardGame
+//
+//  Created by Kevin Zhu on 15/10/25.
+//  Copyright (c) 2015年 Kevin Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
